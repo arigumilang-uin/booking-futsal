d63a90fa7cf5ecb31e1d25bc80d8cb6f2d2520b5
Ari Gumilang <12350110349@students.uin-suska.ac.id>
2025-05-13 16:18:39 +0700

    fix: update DB config for Railway using DATABASE_URL with SSL

--- config/db.js
+++ config/db.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '123',
-  database: process.env.DB_NAME || 'booking_futsal',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // dibutuhkan Railway
+  },
 });
 
 module.exports = pool;
