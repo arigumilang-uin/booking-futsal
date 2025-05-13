32ca800c292486c1fb058ac40be961892a163eb1
Ari Gumilang <12350110349@students.uin-suska.ac.id>
2025-05-13 16:24:44 +0700

    fix: update DB config for Railway using DATABASE_URL with SSL

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // dibutuhkan Railway
+    rejectUnauthorized: false, // untuk Railway/Render/Heroku
   },
 });
 
